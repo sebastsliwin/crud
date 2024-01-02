2f426572f4a8309138eff16ea76ecb3ff69c8022
Sebastian Śliwiński <sxsliwinski@gmail.com>
2024-01-02 18:21:43 +0100

    Feat: Heroku - changed config

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/crud'));
+app.use(express.static(__dirname + '/dist/browser/crud'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/crud/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/crud/browser/index.html'));
 });
 app.listen(process.env.PORT || 8080);
